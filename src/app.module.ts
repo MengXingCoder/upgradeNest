dd8763df417ffdf3c9b018b53fbe58cb2f3cfefb
MnengXingCoder <917660267@qq.com>
2025-12-30 15:19:46 +0800

    feat:配置redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { commonConfigModule } from './config/commonConfig.module';
 import { DatabaseModule } from './commonModules/databaseModules/database.modules';
+import { RedisService } from './redis/redis.service';
 
 
 @Module({
   imports: [commonConfigModule,DatabaseModule],
   controllers: [AppController],
-  providers: [],
+  providers: [RedisService],
 })
 export class AppModule {}
---
+++ src/redis/redis.service.ts
@@ -0,0 +1,25 @@
+
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnModuleDestroy {
+  private client: Redis;
+
+  constructor() {
+    this.client = new Redis({
+      host: 'localhost',
+      port: 6379,
+    //   password: 'example',
+      retryStrategy: () => 1000,
+    });
+  }
+
+  getClient(): Redis {
+    return this.client;
+  }
+
+  async onModuleDestroy() {
+    await this.client.quit();
+  }
+}
